54f75daadac3e73355d5e8cf760e51900ccbc299
TeamMemberComfuture <teammembercomfuture@gmail.com>
2022-06-20 09:44:34 +0200

    uppdate

--- WebApi/Controllers/TaskSaSController.cs
+++ WebApi/Controllers/TaskSaSController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using WebApi.Models;
 using WebApi.Repositories;
@@ -26,6 +27,22 @@ namespace WebApi.Controllers
         public IQueryable<TaskModel> GetListOfTask()
         {
             return _repository.GetAll();
+        }
+        [HttpGet]
+        [Route("OnKonsole")]
+        public IActionResult OnKonsole()
+        {
+            string appPath = @"...";
+            string appArgs = @"...";
+            Process proc = new Process();
+            ProcessStartInfo si = new ProcessStartInfo(appPath, appArgs);
+            si.WindowStyle = ProcessWindowStyle.Normal;
+            si.Verb = "runas";             // UAC elevation required.
+            si.UseShellExecute = true;     // Required for UAC elevation.
+            proc.StartInfo = si;
+            proc.Start();
+            proc.WaitForExit();
+            return Ok();
         }
     }
 }
